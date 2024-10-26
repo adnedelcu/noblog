b20c0befbc683f091529b3fe68dedc28ef4bd23f
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-10-26 14:13:18 +0200

    Added port to configuration

--- config/config.js
+++ config/config.js
@@ -11,6 +11,7 @@ export default {
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   }
 }
