d4b87011a70843011fa4d5c59ddde47fa2873bec
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-10-26 14:05:19 +0200

    Updated config

---
+++ config/config.js
@@ -0,0 +1,16 @@
+export default {
+  "development": {
+    "username": "root",
+    "password": "Alexandru90",
+    "database": "fsb",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+  }
+}
